bff7657a84a5ae28aa5e434b82995f6456d28cc7
Cidree <adrian.cidre@gmail.com>
2024-06-15 11:55:54 +0200

    first commit

---
+++ S1_RStudio/analysis/workspace_example.R
@@ -0,0 +1,16 @@
+
+
+## Do some analysis
+set.seed(123)
+x <- sample(1:20, 1)
+y <- sample(1:20, 1)
+z <- sample(1:20, 1)
+
+## Create results based on previous analysis
+results <- x * y + z
+
+## Export results
+print(results)
+
+
+## .............
